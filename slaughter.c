c1035113f65758d8eaf14b11e59fbc60070f9527
Eric MSP Veith <eveith@gnyu-linux.org>
2014-12-17 13:57:25 +0100

    Init

---
+++ slaughter.c
@@ -0,0 +1,91 @@
+/* slaughter -- A simple program to reliably end a process's life.
+ * Copyright (C) 2014  Eric MSP Veith <eveith@veith-m.de>
+ */
+
+
+#include <stdlib.h>
+#define _GNU_SOURCE 1
+#include <stdio.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+#define DEFAULTWAIT 15
+
+
+void print_usage()
+{
+    printf("sl -- A reliable process terminator\n"
+            "Usage: sl [-s SECS] PID\n\n"
+            "Options:\n\n"
+            "-w SECS    Wait SECS seconds for the process to end\n"
+            "           (Default: 15 seconds)\n");
+}
+
+
+int wait_for_termination(pid_t pid, unsigned seconds)
+{
+    char *path = NULL;
+    struct stat s;
+    int rc;
+
+    asprintf(&path, "/proc/%u", pid);
+
+    while (0 == (rc = stat(path, &s)) && seconds-- > 0) {
+        sleep(1);
+    }
+
+    free(path);
+
+    if (0 == rc) {
+        rc = -1;
+    } else {
+        rc = 0;
+    }
+
+    return rc;
+}
+
+
+int main(int argc, char *argv[])
+{
+    pid_t pid;
+    unsigned waitsecs = DEFAULTWAIT;
+    int waitrc, c;
+
+    while (-1 != (c = getopt(argc, argv, "hs:"))) {
+        switch (c) {
+            case 'w':
+                waitsecs = (unsigned)strtoul(optarg, NULL, 10);
+                break;
+            case 'h':
+                print_usage();
+                exit(EXIT_SUCCESS);
+            default:
+                exit(1);
+        }
+    }
+
+    if (optind == argc) {
+        fprintf(stderr, "ERROR: Need a PID\n");
+        exit(1);
+    }
+
+    pid = (pid_t)strtoul(argv[optind], NULL, 10);
+
+    kill(pid, 15);
+    if (0 == wait_for_termination(pid, waitsecs)) goto success;
+    kill(pid, 2);
+    if (0 == wait_for_termination(pid, waitsecs)) goto success;
+    kill(pid, 1);
+    if (0 == wait_for_termination(pid, waitsecs)) goto success;
+    kill(pid, 9);
+    if (0 == wait_for_termination(pid, waitsecs)) goto success;
+
+    exit(2); /* Could not terminate the process */
+
+success:
+    return EXIT_SUCCESS;
+}
